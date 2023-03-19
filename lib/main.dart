d128631b3b93b50ddc90d218b9a0dfad22fa5064
AzureAD\고명민 <miung222@gmail.com>
2023-03-20 08:08:28 +0900

    theme 추가

--- lib/main.dart
+++ lib/main.dart
@@ -25,29 +25,38 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const Text(
-              'Click Count',
-              style: TextStyle(fontSize: 30),
-            ),
-            // Text(
-            //   '$counter',
-            //   style: const TextStyle(fontSize: 30),
-            // ),
-            for (var n in numbers) Text('$n'),
-            IconButton(
-              iconSize: 40,
-              onPressed: onClicked,
-              icon: const Icon(
-                Icons.add_box_rounded,
-              ),
-            ),
-          ]),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              MyLargeTitle(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'My Large Title',
+      style: TextStyle(fontSize: 30),
     );
   }
 }
